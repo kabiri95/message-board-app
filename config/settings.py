5322e07359462840becdbae9790ede8d65279b95
kabiri95 <erfankabiri337@gmail.com>
2022-07-16 04:29:30 -0600

    new updates for heroku login

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^uuo&hps%s=x077xvz35lag@do)gvg6(o-^dgl94igbzbs(9if'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
